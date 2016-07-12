0c4759866fb6ef08c22e5e260bc2a812cb40dff0
Sarah <xiaojing.si@rea-group.com>
2016-07-12 17:27:23 +0800

    user brower sync instead of live server

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var gls = require('gulp-live-server');
+var browserSync = require('browser-sync');
 
-gulp.task('sass', function () {
-  return gulp.src('./src/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+const reload = browserSync.reload;
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch("src/*.scss", ['sass']);
+    gulp.watch("index.html").on('change', browserSync.reload);
 });
- 
 
-gulp.task('serve', function() {
-  //2. serve at custom port
-  var server = gls.static('./', 3030);
-  // var server = gls('./', true, 3030);
-  server.start();
-
-  //use gulp.watch to trigger server actions(notify, start or stop)
-  gulp.watch(['./src/*.scss', './index.html', './src/images/***'], ['sass'], function (file) {
-    server.notify.apply(server, [file]);
-  });
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("src/*.scss")
+        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("dist"))
+        .pipe(browserSync.stream());
 });
+
+gulp.task('default', ['serve']);
